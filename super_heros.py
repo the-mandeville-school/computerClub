91f95a1d940dc4ab3042e866620d39e89106a898
The Mandeville School <mandeville-school@users.noreply.github.com>
2015-01-23 15:49:28 +0000

    Bug fix - Now can exit game on '0' AttChoice

--- super_heros.py
+++ super_heros.py
@@ -6,7 +6,6 @@
 # 17/12/14  : Added CSV load file functionality
 #             Changed looping mechanism around cards to cater for a variable number of cards.
 
-
 # Import CSV controls
 import csv
 
@@ -65,8 +64,9 @@ for heroID in gameDeck:
 
 # Main block
 # Typical Game round, starts with player 1
+attChoice = -1
 
-while len(p1Cards) > 0 and len(p2Cards) > 0:
+while len(p1Cards) > 0 and len(p2Cards) > 0 and attChoice != 0:
     p1HeroID = p1Cards.pop()
     p2HeroID = p2Cards.pop()
     p1Hero = deck[p1HeroID]
@@ -85,49 +85,49 @@ while len(p1Cards) > 0 and len(p2Cards) > 0:
     attChoice = -1
     while (attChoice < 0 or attChoice > (cardAtts - 1)):
         attChoice = int(raw_input("Choose Attribute number to compare [1-{0}] [0] to quit : ".format((cardAtts - 1))))
+    if attChoice != 0:
+        print ""
+        print "Player one: {0:>{padName1}}     Player two: {1:>{padName2}}".format(p1Hero[0], p2Hero[0], padName1=hero1NameLen + 12, padName2=hero2NameLen + 12)
+        print "=" * (hero1NameLen + 24), "   ", "=" * (hero2NameLen + 24)
+        for attNum in range(1, cardAtts):
+            if attNum == attChoice:
+                print "v" * (hero1NameLen + 24), "   ", "v" * (hero2NameLen + 24)
+            print "{0}: {1:>{padLen1}}     {2}: {3:>{padLen2}}".format(cAtt[attNum], p1Hero[attNum], cAtt[attNum], p2Hero[attNum],
+                padLen1=hero1NameLen + 22 - len(cAtt[attNum]), padLen2=hero2NameLen + 22 - len(cAtt[attNum]))
+            if attNum == attChoice:
+                print "^" * (hero1NameLen + 24), "   ", "^" * (hero2NameLen + 24)
 
-    print ""
-    print "Player one: {0:>{padName1}}     Player two: {1:>{padName2}}".format(p1Hero[0], p2Hero[0], padName1=hero1NameLen + 12, padName2=hero2NameLen + 12)
-    print "=" * (hero1NameLen + 24), "   ", "=" * (hero2NameLen + 24)
-    for attNum in range(1, cardAtts):
-        if attNum == attChoice:
-            print "v" * (hero1NameLen + 24), "   ", "v" * (hero2NameLen + 24)
-        print "{0}: {1:>{padLen1}}     {2}: {3:>{padLen2}}".format(cAtt[attNum], p1Hero[attNum], cAtt[attNum], p2Hero[attNum],
-            padLen1=hero1NameLen + 22 - len(cAtt[attNum]), padLen2=hero2NameLen + 22 - len(cAtt[attNum]))
-        if attNum == attChoice:
-            print "^" * (hero1NameLen + 24), "   ", "^" * (hero2NameLen + 24)
+    # Determine who wins and who loses
+        print "Att Choice {0}".format(attChoice)
+        print "P1: {0}, P2 {1}".format(p1Hero[attChoice], p2Hero[attChoice])
 
-# Determine who wins and who loses
-    print "Att Choice {0}".format(attChoice)
-    print "P1: {0}, P2 {1}".format(p1Hero[attChoice],p2Hero[attChoice])
-
-    if p1Hero[attChoice] > p2Hero[attChoice]:
-        print "=" * len(p1Hero[0] + " beats " + p2Hero[0])
-        print "{0} beats {1}".format(p1Hero[0], p2Hero[0])
-        print "=" * len(p1Hero[0] + " beats " + p2Hero[0])
-        p1Cards.appendleft(p1HeroID)
-        p1Cards.appendleft(p2HeroID)
-#Collect on table Pile of cards if required'
-        while len(pileCards) > 0:
-            p1Cards.appendleft(pileCards.pop())
-    elif p1Hero[attChoice] < p2Hero[attChoice]:
-        print "=" * len(p1Hero[0] + " defeated by " + p2Hero[0])
-        print "{0} defeated by {1}".format(p1Hero[0], p2Hero[0])
-        print "=" * len(p1Hero[0] + " defeated by " + p2Hero[0])
-        p2Cards.appendleft(p2HeroID)
-        p2Cards.appendleft(p1HeroID)
-#Collect on table Pile of cards if required'
-        while len(pileCards) > 0:
-            p2Cards.appendleft(pileCards.pop())
-    else:
-        print "Draw, add cards to unclaimed pile"
-        pileCards.append(p1HeroID)
-        pileCards.append(p2HeroID)
-        for pileID in pileCards:
-            pHero = deck[pileID]
-            print "{0}".format(pHero[0])
-# List in-hand card quantity after round
-    print "Cards in hand, player one: {0} v player two {1}".format(len(p1Cards), len(p2Cards))
+        if p1Hero[attChoice] > p2Hero[attChoice]:
+            print "=" * len(p1Hero[0] + " beats " + p2Hero[0])
+            print "{0} beats {1}".format(p1Hero[0], p2Hero[0])
+            print "=" * len(p1Hero[0] + " beats " + p2Hero[0])
+            p1Cards.appendleft(p1HeroID)
+            p1Cards.appendleft(p2HeroID)
+    #Collect on table Pile of cards if required'
+            while len(pileCards) > 0:
+                p1Cards.appendleft(pileCards.pop())
+        elif p1Hero[attChoice] < p2Hero[attChoice]:
+            print "=" * len(p1Hero[0] + " defeated by " + p2Hero[0])
+            print "{0} defeated by {1}".format(p1Hero[0], p2Hero[0])
+            print "=" * len(p1Hero[0] + " defeated by " + p2Hero[0])
+            p2Cards.appendleft(p2HeroID)
+            p2Cards.appendleft(p1HeroID)
+    #Collect on table Pile of cards if required'
+            while len(pileCards) > 0:
+                p2Cards.appendleft(pileCards.pop())
+        else:
+            print "Draw, add cards to unclaimed pile"
+            pileCards.append(p1HeroID)
+            pileCards.append(p2HeroID)
+            for pileID in pileCards:
+                pHero = deck[pileID]
+                print "{0}".format(pHero[0])
+    # List in-hand card quantity after round
+        print "Cards in hand, player one: {0} v player two {1}".format(len(p1Cards), len(p2Cards))
 print "Game Over"
 
     # List entire hands of both players, in order of play
